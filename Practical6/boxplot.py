883c89cad13411482b66f04d86446004af66a525
wmy20030619 <102129325+wmy20030619@users.noreply.github.com>
2022-03-25 11:38:35 +0800

    Add files via upload

--- Practical6/boxplot.py
+++ Practical6/boxplot.py
@@ -1,1 +1,20 @@
 
+#input new students' scores
+marks=input("please input your scores")
+#change the input into integers
+a = marks.split(' ')
+b=[]
+for i in range(len(a)):
+    b.append(int(a[i]))
+print(sorted(b))
+#calclate the mean
+import numpy as np
+A=np.mean(b)
+#judge whether the mean is above 60
+if A>=60:
+    print("the average is over 60,pass")
+else: print("the average is below 60,fail")
+#print the plot
+import matplotlib.pyplot as plt
+plt.boxplot(b)
+plt.show()
